99b77f5fa8e20379de8ad236498d01576d58f4a0
Nils Makander <makander@gmail.com>
2019-04-22 11:31:38 +0200

    change tables for migrations

--- app/Recipe.php
+++ app/Recipe.php
@@ -2,20 +2,13 @@
 
 namespace App;
 
-<<<<<<< HEAD
-=======
 use App\User;
->>>>>>> tmp
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
     protected $fillable = ['user_id', 'yummly_recipe_id', 'recipe_name', 'image_url'];
 
-<<<<<<< HEAD
-=======
-
->>>>>>> tmp
     public function user()
     {
         return $this->belongsTo(User::class);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 30);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 20);
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
